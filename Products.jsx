dcfb82008de478a8f9215506844d997b7f30930e
nursinbulbul <220408041@st.biruni.edu.tr>
2026-01-13 21:00:24 +0300

    Created Products listing page

---
+++ Products.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import products from "../data/products";
+import ProductCard from "../components/ProductCard";
+
+
+function Products() {
+const [search, setSearch] = useState("");
+
+
+return (
+<div className="container">
+<h1>All Products</h1>
+<input
+placeholder="Search"
+onChange={(e) => setSearch(e.target.value)}
+/>
+<div className="grid">
+{products
+.filter((p) => p.name.toLowerCase().includes(search.toLowerCase()))
+.map((p) => (
+<ProductCard key={p.id} product={p} />
+))}
+</div>
+</div>
+);
+}
+
+
+export default Products;
