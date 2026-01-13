1445c3e71367a35f79ce5516c409551108b50c18
SelinBayrak0 <selin2002bayrak@gmail.com>
2026-01-13 21:02:07 +0300

    Added initial product data
    Updated product list

---
+++ products.js
@@ -0,0 +1,81 @@
+const products = [
+    {
+    id: 1,
+    name: "Flower Necklace",
+    price: 100,
+    description: "This elegant silver flower necklace is designed with delicate white and blue details that bring a fresh and feminine touch to any outfit. Its refined craftsmanship makes it perfect for both daily wear and special occasions.",
+    image: "/images/cıcek.jpg"
+    },
+    {
+    id: 2,
+    name: "Butterfly Necklace",
+    price: 100,
+    description: "Inspired by the beauty of nature, this butterfly necklace features a minimal yet eye-catching silver design. It adds a graceful and modern accent, making it an ideal choice for those who love simple but meaningful jewelry.",
+    image: "/images/taşlıkelebek.jpg"
+    },
+    {
+    id: 3,
+    name: "Galaxy Necklace",
+    price: 100,
+    description: "The Galaxy Necklace reflects the mystery and elegance of the universe with its unique and luminous design. It is a timeless piece that stands out beautifully and enhances your style with a subtle sparkle.",
+    image: "/images/sarıgalaksı.jpg"
+    },
+    {
+    id: 4,
+    name: "Rose Necklace",
+    price: 100,
+    description: "This rose-inspired necklace symbolizes elegance and romance with its graceful form and refined details. Designed to highlight your femininity, it is a perfect accessory to complete both casual and elegant looks.",
+    image: "/images/gul.jpg"
+    },
+    {
+    id: 5,
+    name: "Ocean Necklace",
+    price: 100,
+    description: "Inspired by the calming beauty of the ocean, this necklace features a sophisticated design that brings freshness and serenity to your style. Its elegant look makes it a versatile piece suitable for any occasion.e",
+    image: "/images/okyanus.jpg"
+    },
+    {
+        id: 6,
+        name: "Tennis Bracelet",
+        price: 120,
+        description:
+          "This classic tennis bracelet features a timeless design that adds elegance and sparkle to your wrist. Its refined structure makes it a perfect choice for both everyday wear and special occasions.",
+        image: "/images/suyolu.jpg"
+    },
+    {
+        id: 7,
+        name: "Tennis Bracelet",
+        price: 120,
+        description:
+          "Designed with a modern and sophisticated touch, this tennis bracelet offers a graceful shine that complements any outfit. It is an ideal accessory for those who appreciate minimalist luxury.",
+        image: "/images/suyolum.jpg"
+    },
+    {
+        id: 8,
+        name: "Colourful Heart Bracelet",
+        price: 120,
+        description:
+          "This colourful heart bracelet brings joy and vibrancy to your style with its playful and charming design. The lively colors make it a standout accessory, perfect for adding a cheerful accent to your look.",
+        image: "/images/renklı.jpg"
+    },
+    {
+        id: 9,
+        name: "Silver Heart Bracelet",
+        price: 120,
+        description:
+          "The silver heart bracelet features a delicate and elegant design that symbolizes love and simplicity. Its subtle shine makes it a timeless piece suitable for both casual and elegant outfits.",
+        image: "/images/beyaz.jpg"
+    },
+    {
+        id: 10,
+        name: "Heart and Flower Bracelet",
+        price: 120,
+        description:
+          "Combining heart and flower motifs, this bracelet represents elegance and femininity in a unique design. It is crafted to add a soft and romantic touch to your style, making it perfect for any occasion.",
+        image: "/images/sarı.jpg"
+    }
+      
+];
+    
+    
+export default products;
