55762aace05dabbf352a49a39dd473543ac668c2
nursinbulbul <220408041@st.biruni.edu.tr>
2026-01-13 21:01:58 +0300

    Created Product Card component

---
+++ ProductCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ProductCard({ product }) {
+  return (
+    <Link to={`/product/${product.id}`} className="card-link">
+      <div className="card">
+        <img src={product.image} alt={product.name} className="product-image"/>
+        <h3>{product.name}</h3>
+        <p>${product.price}</p>
+      </div>
+    </Link>
+  );
+}
+
+export default ProductCard;
